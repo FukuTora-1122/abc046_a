6376519b30bb710c00f202b6c39dffd3487f711c
FukuTora <61640701+FukuTora-1122@users.noreply.github.com>
2020-03-04 02:04:27 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b >> c;
+    
+    if (a == b && a == c && b == c) {
+        cout << "1" << endl;
+    }else if(a == b || a == c || b == c){
+        cout << "2" << endl;
+    }else{
+        cout << "3" << endl;
+    }
+    return 0;
+}
